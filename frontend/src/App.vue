98f731c7c6c17e42da5774c425478fc7f5761e7f
Krystian <kryman0@gmail.com>
2025-05-16 16:20:32 +0200

    Added vue3 with vite to the project with some init components.

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navigator from "@/components/Navigator.vue";
+</script>
+
+<template>
+  <header>
+    <Navigator />
+  </header>
+  <main>
+
+  </main>
+  <RouterView />
+</template>
---
+++ frontend/src/components/Navigator.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav>
+    <ul className="text-right">
+      <li className="xs:pr-10"><RouterLink className="router-link xs:pr-10" to="/login">Login</RouterLink></li>
+      <li className=""><RouterLink className="router-link" to="/admin">Admin</RouterLink></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+li {
+    @media (min-width: 320px) {
+      display: inline-block;
+    }
+
+    border: 1px solid green;
+  }
+</style>
